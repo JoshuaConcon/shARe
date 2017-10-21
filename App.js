4906c9aaa5d3880059a0936c173a9fe468b1b88d
Vincent Teng <vincent@Vincents-MacBook-Pro.local>
2017-10-21 02:15:28 -0400

    App.js changes

--- App.js
+++ App.js
@@ -3,7 +3,6 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
 import React, { Component } from 'react';
 import {
   Platform,
@@ -11,6 +10,13 @@ import {
   Text,
   View
 } from 'react-native';
+import restRequest from './restRequestClient'
+import Link from 'link-react';
+import { Table } from 'semantic-ui-react';
+import awsmobile from './configuration/aws-exports';
+const cloud_logic_array = JSON.parse(awsmobile.aws_cloud_logic_custom)
+const endPoint = cloud_logic_array[0].endpoint
+const apiRestaurantUri = endPoint + "/items/restaurants";
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -19,23 +25,65 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-export default class App extends Component<{}> {
-  render() {
+class App extends Component {
+  state = {
+    data: []
+  }
+  fetch = () => {
+    let requestParams = {
+      method: 'GET',
+      url: apiRestaurantUri
+    }
+    this.restResponse = restRequest(requestParams)
+      .then(resp => {
+          this.setState({
+            data: resp
+        });
+
+      })
+    .catch (function(error){
+      alert(error);
+  });
+}
+
+//render logic below{
+render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <div className="App">
+        <Link onClick={this.fetch}>
+          List restaurants
+        </Link>
+        <div>
+          <div>
+        {(
+      <Table>
+          <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Name</Table.HeaderCell>
+            <Table.HeaderCell>Address</Table.HeaderCell>
+            <Table.HeaderCell>Contact</Table.HeaderCell>
+            <Table.HeaderCell>Rating</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {this.state.data.map((data, idx) =>
+            <Table.Row key={idx}>
+              <Table.Cell>{data.name}</Table.Cell>
+              <Table.Cell>{data.address}</Table.Cell>
+              <Table.Cell>{data.phone}</Table.Cell>
+              <Table.Cell>{data.rating}</Table.Cell>
+            </Table.Row>
+          )}
+        </Table.Body>
+      </Table>
+    )}
+    </div>
+        </div>
+      </div>
     );
   }
 }
+export default App;
 
 const styles = StyleSheet.create({
   container: {
