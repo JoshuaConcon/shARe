3f2ad4a6e7e7b86da40f22555c05335083e1cefe
Joshua Concon <conconjoshua@gmail.com>
2017-10-21 03:16:34 -0400

    attempting interaction with AR Object

--- index.ios.js
+++ index.ios.js
@@ -1,22 +1,28 @@
 // index.ios.js
 import React, { Component } from 'react';
-import { AppRegistry, View } from 'react-native';
+import { AppRegistry, View, TouchableOpacity } from 'react-native';
 import { ARKit } from 'react-native-arkit';
 export default class App extends Component {
+
+  _onPressAR () {
+    console.log("AR Object Pressed!")
+  }
+
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <ARKit
-        style={{ flex: 1 }}
-        debug // debug mode will show feature points detected and 3D axis
-        planeDetection // turn on plane detection
-        lightEstimation // turn on light estimation
-      >
-      <ARKit.Pyramid
-      pos={{ x: 0, y: 0, z: 0 }}
-      shape={{ width: 0.5, height: 0.5, length: 0.5 }}
-    />
-      </ARKit>
+        <TouchableOpacity onPress={this._onPressAR}>
+          <ARKit
+          style={{ flex: 1 }}
+          debug // debug mode will show feature points detected and 3D axis
+          planeDetection // turn on plane detection
+          lightEstimation // turn on light estimation
+          >
+            <ARKit.Pyramid
+            pos={{ x: 0, y: 0, z: 0 }}
+            shape={{ width: 0.5, height: 0.5, length: 0.5 }} />
+          </ARKit>
+        </TouchableOpacity>
       </View>
     );
   }
